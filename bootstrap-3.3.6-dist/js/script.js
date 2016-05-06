c3982a624b1c7b7a6587cfb8e492f0b6ad2e10aa
loganhendricks <lhendricks88@gmail.com>
2016-05-06 11:12:43 -0600

    pretty much finished

--- bootstrap-3.3.6-dist/js/script.js
+++ bootstrap-3.3.6-dist/js/script.js
@@ -4,9 +4,11 @@
 var card = {
     fname: "Logan",
     lname: "Hendricks",
-    occupation: "Zions Bank - Teller III",
-    pnumber: "(801) 433 - 8040",
-    email: "Logan.L.Hendricks@gmail.com"
+    occupation: "<span>Zions Bank - Teller III</span>",
+    pnumber: "<span>(801) 433 - 8040</span>",
+    email: "<span>Logan.L.Hendricks@gmail.com</span>",
+    website: "<span>www.LoganLHendricks.com</span>",
+    linkedIn: "<a href='https://www.linkedin.com/in/logan-hendricks-a8056138'> <img src='../../images/social/linkedin-logo.png'/> </a>"
 };
 
 document.getElementById("firstName").innerHTML = card.fname;
@@ -14,6 +16,8 @@ document.getElementById("lastName").innerHTML = card.lname;
 document.getElementById("occupation").innerHTML = card.occupation;
 document.getElementById("phone").innerHTML = card.pnumber;
 document.getElementById("email").innerHTML = card.email;
+document.getElementById("website").innerHTML = card.website;
+document.getElementById("linkedIn").innerHTML = card.linkedIn;
 
 
 function moveCard () {
@@ -25,3 +29,27 @@ function textFade (idin, idout) {
     document.getElementById(idin).style.opacity = "0";
     document.getElementById(idout).style.opacity = "1.0";
 }
+
+function tableFade (idForFade) {
+    for (var x in idForFade) {
+      //  if (i = 0; i < idForFade.length; i++) {
+            document.getElementById(idForFade).style.opacity = "1.0";
+  //      }
+    }
+}
+
+function imgtextAppear(idtoAppear) {
+    document.getElementById(idtoAppear).style.opacity = "1.0";
+}
+
+function imgtextFade (idtoFade) {
+    document.getElementById(idtoFade).style.opacity = "0";
+}
+
+
+
+
+
+
+
+
