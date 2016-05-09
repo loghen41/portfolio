86971227776e068c92a18f62112cc00b6f21f7fe
loganhendricks <lhendricks88@gmail.com>
2016-05-09 11:36:43 -0600

    pretty much finished

--- bootstrap-3.3.6-dist/js/script.js
+++ bootstrap-3.3.6-dist/js/script.js
@@ -8,7 +8,8 @@ var card = {
     pnumber: "<span>(801) 433 - 8040</span>",
     email: "<span>Logan.L.Hendricks@gmail.com</span>",
     website: "<span>www.LoganLHendricks.com</span>",
-    linkedIn: "<a href='https://www.linkedin.com/in/logan-hendricks-a8056138'> <img src='../../images/social/linkedin-logo.png'/> </a>"
+    linkedIn: "<a href='https://www.linkedin.com/in/logan-hendricks-a8056138'> <img src='../images/social/linkedin-logo.png'/> </a>",
+    facebook: "<a href='https://www.facebook.com/logan.hendricks.756'> <img src='../images/social/facebook-letter-logo.png'/> </a>"
 };
 
 document.getElementById("firstName").innerHTML = card.fname;
@@ -18,7 +19,6 @@ document.getElementById("phone").innerHTML = card.pnumber;
 document.getElementById("email").innerHTML = card.email;
 document.getElementById("website").innerHTML = card.website;
 document.getElementById("linkedIn").innerHTML = card.linkedIn;
-
 
 function moveCard () {
     document.getElementById("dbcard").style.marginTop = "5%";
